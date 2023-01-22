924a92eaae3a50eab99064ca716f98d33b6dd366
Yun Chen <Yunchenfly@gmail.com>
2023-01-22 03:49:33 -0500

    added tagline

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.querySelector('.beer-button')
     const beerButton = document.querySelector('.random-beer')
+    const tagLine = document.querySelector('.tag-line')
     function getRandomBeer(beers) {
         const randomId = Math.floor(Math.random() * 25) + 1
         let randomBeer
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => {
             const randomBeer = getRandomBeer(data)
             beerButton.innerHTML = randomBeer.name
+            tagLine.innerHTML = randomBeer.tagline 
             console.log(randomBeer)
            
            
