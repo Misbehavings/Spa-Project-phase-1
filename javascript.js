c5ff982981f65018738f1b2ee10ebf007be6c0f1
Yun Chen <Yunchenfly@gmail.com>
2023-01-22 03:43:39 -0500

    added function for random beer generator

--- javascript.js
+++ javascript.js
@@ -1,10 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    function getRandomBeer(beers) {
+        const randomId = Math.floor(Math.random() * 25) + 1
+        let randomBeer
+        beers.forEach(beer => {
+            if (beer.id === randomId) {
+                randomBeer = beer;
+                return;
+            }
+        })
+        return randomBeer
+    }
     
-    fetch('https://api.punkapi.com/v2/beers')
-        .then(response => {
+    function getData() {
+        fetch('https://api.punkapi.com/v2/beers')
+            .then(response => {
             return response.json()
         })
-        .then(data => {
-            console.log(data)
+            .then(data => {
+           
+           
+           
         })
+
+    }
+
+
+
 })
+
+
